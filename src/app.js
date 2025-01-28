b17084594ef933ebce370f4871da7341065b84a5
sandovalaes <sandoval.aes@gmail.com>
2025-01-28 20:37:18 -0300

    Primer Commit

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const PORT = 8080
+//const productsRouter = require("./routes/products.router")
+//const cartRouter = require("./routes/carts.router")
+
+//app.use(express.json())
+//app.use(express.urlencoded({extended : true}))
+
+//app.use(express.static(path.join(__dirname,'public')))
+
+//app.use("/api",productsRouter)
+//app.use("/api",cartRouter)
+
+app.get('/',(req,res) =>{res.send('Mi primer servidor')})
+
+app.listen(PORT, ()=>{
+    console.log(`Servidor corriendo sobre el puerto ${PORT}`)
+})
